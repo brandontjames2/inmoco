89fe32c1845c03259daf115f62fcc446600f5fbf
Brandon James <brandon.james@slalom.com>
2019-12-10 14:46:16 -0500

    added routing to shop component and changed sidenav style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'shop', component: ShopComponent}
 ];
 
 @NgModule({
