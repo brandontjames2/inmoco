bececd6b61a367c14ea8be9d3697c5711794d878
Brandon James <brandon.james@slalom.com>
2019-12-10 16:13:44 -0500

    added more styling to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { ShopItem } from '../shop/shop.model';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,51 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+    shopItems: ShopItem[] = [];
 
   ngOnInit() {
+    this.shopItems = [
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        },
+        {
+            title: "shoe",
+            price: 99,
+            imagePath: "../../assets/img/logo2.jpeg"
+        }
+    ]
   }
 
   ngOnDestroy() {
