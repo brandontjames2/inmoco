b0f22a422742abe363afcdc4d7d66e47b4f2bffa
Brandon James <brandon.james@slalom.com>
2019-12-10 16:25:37 -0500

    fixing references

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ShopItem } from '../shop-item/shop.model';
+import { ShopItem } from '../shop/shop.model';
 
 @Component({ 
   selector: 'app-header',
